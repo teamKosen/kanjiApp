5bd6334a5a068b654aee5c016afa5fc51a113897
山下純一 <metarusword1998@gmail.com>
2021-07-14 17:04:26 +0900

    目的単体での検索

--- pages/timeline.jsx
+++ pages/timeline.jsx
@@ -3,6 +3,8 @@ import style from '../styles/timeline.module.scss'
 import { TextField } from '@material-ui/core';
 
 import { Post } from '../components/timeline/post/post';
+import { Button , InputBase } from '@material-ui/core';
+import { useState, useEffect } from 'react';
 
 export async function getStaticProps(context) {
     const res = await fetch("http://localhost:3000/api/shopdatas");
@@ -16,27 +18,51 @@ export async function getStaticProps(context) {
 }
 
 const Timeline = ({shopdatas}) => {
-    
+    const [keyword, setKeyword] = useState('');
+    const [shops, updateShops] = useState(shopdatas);
     const shoplist = JSON.parse(JSON.stringify(shopdatas));
+
+    useEffect(() => {
+        if (keyword === '') return
+    
+        const purpose = keyword;
+    
+        const request = async () => {
+          const res = await fetch("http://localhost:3000/api/testshopdatas?purpose="+purpose)
+          const shopdatas= await res.json()
+          console.log(shopdatas);
+          updateShops(shopdatas);
+        }
+        request()
+      }, [keyword])
+
+    const handlerOnSubmitSearch = (e) => {
+        e.preventDefault()
+    
+        const { currentTarget = {} } = e
+        const fields = Array.from(currentTarget?.elements)
+        const fieldQuery = fields.find((field) => field.name === 'query')
+    
+        const value = fieldQuery.value || ''
+        setKeyword(value);
+        console.log(keyword);
+    }    
     
     return (
         <div className= {style.timeline}>
             <h1>Dlink</h1>
             <div className={style.searchField}>
-                <TextField
-                    id="outlined-full-width"
-                    label="Search"
-                    className={style.searchFieldInput}
-                    placeholder="検索条件を入力してください"
-                    margin="normal"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    variant="outlined"
-                    type="text"
-                />
+                <form onSubmit={handlerOnSubmitSearch}>
+                    <input
+                        type="search"
+                        name="query"
+                        className={style.searchFieldInput}
+                        placeholder="キーワードを入力して下さい"
+                    />
+                    <Button>Search</Button>
+                </form>
             </div>
-            {shoplist.map((shopdata) => (
+            {shops.map((shopdata) => (
                 <div key={shopdata.name} className={style.post}>
                     <Post 
                         name={shopdata.name}
