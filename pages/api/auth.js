0f574a8f322493b30b0b11da315ca7b3bed135a7
山下純一 <metarusword1998@gmail.com>
2021-09-05 17:45:56 +0900

    _id取得

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -2,6 +2,7 @@ import nextConnect from "next-connect";
 import middleware from "../../middleware/database";
 import passport from "../../middleware/passport";
 import { extractUser } from "../../lib/api-helpers";
+import { extractCurrentUser } from "../../lib/api-currentUser-helper";
 
 const handler = nextConnect();
 
@@ -9,7 +10,8 @@ handler.use(middleware);
 
 handler.post(passport.authenticate("local"), (req, res) => {
   // return our user object
-  res.json({ user: extractUser(req.user) });
+  // res.json({ user: extractUser(req.user) });
+  res.json({ user: extractCurrentUser(req.user) });
 });
 
 handler.delete((req, res) => {
