d7d67ec6007500337383d6be7424a9183402bef6
Okamura-Yuusuke <okka1521@gmail.com>
2021-12-01 16:05:30 +0900

    オファー投稿機能実装

--- components/shopplan/components/create-offer-modal/create-offer-modal.tsx
+++ components/shopplan/components/create-offer-modal/create-offer-modal.tsx
@@ -2,6 +2,9 @@ import { TextField, Button,InputAdornment, Dialog, DialogActions, DialogContent,
 import {useStyles} from './create-offer-modal.style'
 import React, { useState, useEffect,FunctionComponent, useCallback } from 'react'
 import { red } from '@material-ui/core/colors'
+import Tag from "./components/tag";
+
+import { useUser } from "../../../../lib/hooks";
 
 type Props = {
     handleCreatePlanOpen: () => void;
@@ -13,45 +16,66 @@ type Props = {
 export const CreateOfferModal:FunctionComponent<Props> = (props) => {
     const { handleCreatePlanOpen, handleCreatePlanClose, isOpenCreatePlan, plandata } = props;
     const style = useStyles();
+
+    const [user, { mutate }] = useUser();
+    const [errorMsg, setErrorMsg] = useState("");
+    const [tag, setTags] = useState([]);
+
+    const handleSubmit = async (e) => {
+        const today = new Date();
+        e.preventDefault();
+        const body = {
+            title: e.currentTarget.title.value,
+            budget: e.currentTarget.budget.value,
+            numberOfPeople: e.currentTarget.numberOfPeople.value,
+            tag: tag,
+            place: e.currentTarget.place.value,
+            userPlanId: plandata._id,
+            comment: e.currentTarget.comment.value,
+            offerState: 1,
+            openDate: e.currentTarget.openDate.value,
+            openTime: e.currentTarget.openTime.value,
+            closeDate: e.currentTarget.closeDate.value,
+            closeTime: e.currentTarget.closeTime.value,
+            create_at: today,
+            update_at: today,
+            user: user,
+        };
+        const res = await fetch("api/create_offer", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+        });
+        setErrorMsg(await res.text());
+    }
+
     return (
         <div>
             <Dialog
                 open={isOpenCreatePlan}
                 onClose={handleCreatePlanClose}
             >
-                <>{plandata?.title}</>
                 <DialogTitle id="alert-dialog-title">{"幹事にオファーする"}</DialogTitle>
                 <DialogContent>
-                    <form>
-                        {/* {errorMsg ? <p style={{color: "red"}}>{errorMsg}</p> : null} */}
-                        <div className={style.formtitle}>
-                            <div>お店の名前を入力してください</div>
-                            <div className={style.formtitleAnnotation}><span style={{color: "red"}}>*</span>必須</div>
-                        </div>
+                    <form onSubmit={handleSubmit}>
+                        {errorMsg ? <p style={{color: "red"}}>{errorMsg}</p> : null}
                         <div className={style.form}>
-                            <TextField style={{width:"100%"}} size="small" variant="outlined" id="shopname" name="shopname" type="text" label="お店の名前"/>
+                            <TextField style={{width:"80%"}} size="small" variant="outlined" id="title" name="title" type="text" label="タイトル"/>
                         </div>
-                        <div className={style.formtitle}>
-                            <div>お店の場所を入力してください</div>
-                            <div className={style.formtitleAnnotation}><span style={{color: "red"}}>*</span>必須</div>
-                        </div>
+
                         <div className={style.form}>
-                            <TextField style={{width:"100%"}} size="small" variant="outlined" id="place" name="place" type="text" label="場所"/>
+                            <Tag style={{width:"80%"}} size="small" variant="outlined" setValue={setTags}/>
+                        </div>
+
+                        <div className={style.form}>
+                            <TextField size="small" variant="outlined" id="place" name="place" type="text" label="場所"/>
                         </div> 
-                        <div className={style.formtitle}>
-                            <div>人数/予算を入力してください</div>
-                            <div className={style.formtitleAnnotation}><span style={{color: "red"}}>*</span>必須</div>
-                        </div>
+
                         <div className={style.form}>
                             <TextField size="small" variant="outlined" id="numberOfPeople" name="numberOfPeople" type="Number" label="人数" InputProps={{endAdornment:<InputAdornment position="end">人</InputAdornment>}}/>
-                        </div>
-                        <div className={style.form}>
                             <TextField size="small" variant="outlined" id="budget" name="budget" type="Number" label="予算" InputProps={{endAdornment:<InputAdornment position="end">円</InputAdornment>}}/>
                         </div>
-                        <div className={style.formtitle}>
-                            <div>開催日時/終了日時を入力してください</div>
-                            <div className={style.formtitleAnnotation}><span style={{color: "red"}}>*</span>必須</div>
-                        </div>
+                        
                         <div className={style.form}>
                             <div style={{paddingBottom:"5px"}}>開催日時</div>
                             <TextField label="日付" size="small" variant="outlined" id="openDate" name="openDate" type="date" InputLabelProps={{shrink: true}}/>
@@ -63,22 +87,11 @@ export const CreateOfferModal:FunctionComponent<Props> = (props) => {
                             <TextField label="日付" size="small" variant="outlined" id="closeDate" name="closeDate" type="date" InputLabelProps={{shrink: true}}/>
                             <TextField label="時間" size="small" variant="outlined" id="closeTime" name="closeTime" type="time" InputLabelProps={{shrink: true}}/>
                         </div>
-                        <div className={style.formtitle}>
-                            <div>{"連作先を入力してください(任意)"}</div>
-                        </div>
-                        <div className={style.form}>
-                            <TextField style={{width:"100%"}} size="small" variant="outlined" id="tel" name="tel" type="tel" label="tel"/>
-                        </div> 
-
-                        <div className={style.form}>
-                            <TextField style={{width:"100%"}} size="small" variant="outlined" id="URL" name="URL" type="text" label="URL"/>
-                        </div> 
-                        <div className={style.formtitle}>
-                            <div>{"コメント欄(割引やクーポンの詳細など)"}</div>
-                        </div>
+                        
                         <div className={style.form}>
                             <TextField style={{width:"100%"}} size="small" variant="outlined" multiline rows={5} id="comment" name="comment" type="text" label="コメント"/>
                         </div>
+
                         <div className={style.button}>
                             <Button onClick={handleCreatePlanClose}>←戻る</Button>
                             <Button variant="contained" type="submit" color="secondary">確定</Button>
