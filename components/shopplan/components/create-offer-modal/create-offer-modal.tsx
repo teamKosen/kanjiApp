a1ae65e8a0063b243495c12d45d3911d2df3b830
Okamura-Yuusuke <okka1521@gmail.com>
2021-12-03 16:23:46 +0900

    オファー作成モーダルのデザイン調整

--- components/shopplan/components/create-offer-modal/create-offer-modal.tsx
+++ components/shopplan/components/create-offer-modal/create-offer-modal.tsx
@@ -27,7 +27,6 @@ export const CreateOfferModal:FunctionComponent<Props> = (props) => {
         const today = new Date();
         e.preventDefault();
         const body = {
-            title: e.currentTarget.title.value,
             budget: e.currentTarget.budget.value,
             numberOfPeople: e.currentTarget.numberOfPeople.value,
             tag: tag,
@@ -70,35 +69,77 @@ export const CreateOfferModal:FunctionComponent<Props> = (props) => {
                 <DialogContent>
                     <form onSubmit={handleSubmit}>
                         {errorMsg != "ok" ? <p style={{color: "red"}}>{errorMsg}</p> : null}
+<<<<<<< HEAD
                         <div className={style.form}>
                             <TextField style={{width:"80%"}} size="small" variant="outlined" id="title" name="title" type="text" label="タイトル" value={plandata?.title} />
+=======
+                        {/* タグ */}
+                        <div className={style.formtitle}>
+                            <div>タグを設定してください</div>
+                            <div className={style.formtitleAnnotation}><span style={{color: "red"}}>*</span>必須</div>
+>>>>>>> オファー作成モーダルのデザイン調整
                         </div>
-
                         <div className={style.form}>
                             <Tag style={{width:"80%"}} size="small" variant="outlined" setValue={setTags}/>
                         </div>
-
+                        
+                        {/* 場所 */}
+                        <div className={style.formtitle}>
+                            <div>お店の場所を入力してください</div>
+                            <div className={style.formtitleAnnotation}><span style={{color: "red"}}>*</span>必須</div>
+                        </div>
                         <div className={style.form}>
                             <TextField size="small" variant="outlined" id="place" name="place" type="text" label="場所" value={plandata?.place}/>
                         </div> 
 
+                        {/* 人数/予算 */}
+                        <div className={style.formtitle}>
+                            <div>人数/予算を入力してください</div>
+                            <div className={style.formtitleAnnotation}><span style={{color: "red"}}>*</span>必須</div>
+                        </div>
                         <div className={style.form}>
                             <TextField size="small" variant="outlined" id="numberOfPeople" name="numberOfPeople" type="Number" label="人数" value={plandata?.numberOfPeople} InputProps={{endAdornment:<InputAdornment position="end">人</InputAdornment>}}/>
                             <TextField size="small" variant="outlined" id="budget" name="budget" type="Number" label="予算" value = {plandata?.budget} InputProps={{endAdornment:<InputAdornment position="end">円</InputAdornment>}}/>
                         </div>
                         
+                        {/* 開始時間 */}
+                        <div className={style.formtitle}>
+                            <div>開催日時を設定してください</div>
+                            <div className={style.formtitleAnnotation}><span style={{color: "red"}}>*</span>必須</div>
+                        </div>
                         <div className={style.form}>
+<<<<<<< HEAD
                             <div style={{paddingBottom:"5px"}}>開催日時</div>
                             <TextField label="日付" size="small" variant="outlined" id="openDate" name="openDate" type="date" defaultValue={plandata?.openTime.substring(0,10)} InputLabelProps={{shrink: true}}/>
                             <TextField label="時間" size="small" variant="outlined" id="openTime" name="openTime" type="time" defaultValue={plandata?.openTime.substring(11,16)} InputLabelProps={{shrink: true}}/>
+=======
+                            {/*<div style={{paddingBottom:"5px"}}>開催日時</div>*/}
+                            <TextField label="日付" size="small" variant="outlined" id="openDate" name="openDate" type="date" InputLabelProps={{shrink: true}}/>
+                            <TextField label="時間" size="small" variant="outlined" id="openTime" name="openTime" type="time" InputLabelProps={{shrink: true}}/>
+>>>>>>> オファー作成モーダルのデザイン調整
                         </div>
                         
+                        {/* 終了日時 */}
+                        <div className={style.formtitle}>
+                            <div>終了日時を設定してください</div>
+                            <div className={style.formtitleAnnotation}><span style={{color: "red"}}>*</span>必須</div>
+                        </div>
                         <div className={style.form}>
+<<<<<<< HEAD
                             <div style={{paddingBottom:"5px"}}>終了日時</div>
                             <TextField label="日付" size="small" variant="outlined" id="closeDate" name="closeDate" type="date" defaultValue={plandata?.closeTime.substring(0,10)} InputLabelProps={{shrink: true}}/>
                             <TextField label="時間" size="small" variant="outlined" id="closeTime" name="closeTime" type="time" defaultValue={plandata?.closeTime.substring(11,16)} InputLabelProps={{shrink: true}}/>
+=======
+                            {/*<div style={{paddingBottom:"5px"}}>終了日時</div>*/}
+                            <TextField label="日付" size="small" variant="outlined" id="closeDate" name="closeDate" type="date" InputLabelProps={{shrink: true}}/>
+                            <TextField label="時間" size="small" variant="outlined" id="closeTime" name="closeTime" type="time" InputLabelProps={{shrink: true}}/>
+>>>>>>> オファー作成モーダルのデザイン調整
                         </div>
                         
+                        {/* コメント */}
+                        <div className={style.formtitle}>
+                            <div>コメントを設定してください</div>
+                        </div>
                         <div className={style.form}>
                             <TextField style={{width:"100%"}} size="small" variant="outlined" multiline rows={5} id="comment" name="comment" type="text" label="コメント"/>
                         </div>
