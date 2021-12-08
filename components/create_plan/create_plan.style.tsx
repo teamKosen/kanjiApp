dbfafdb64d2a4c49252415285314cbae5ad8bc00
nogissyu <noguchi.ryousuke977@mail.kyutech.jp>
2021-12-08 16:55:17 +0900

    タグの調整

--- components/create_plan/create_plan.style.tsx
+++ components/create_plan/create_plan.style.tsx
@@ -54,7 +54,7 @@ export const useStyles = makeStyles((theme) =>
 
     boxdesign: {
       width: "100%",
-      height: "44px",
+      lineHeight: "44px",
       fontSize: "24px",
       background: "#FFFFFF",
       [`& fieldset`]: {
@@ -65,7 +65,7 @@ export const useStyles = makeStyles((theme) =>
 
     boxdesigndate: {
       width: "50%",
-      height: "44px",
+      lineHeight: "44px",
       fontSize: "24px",
       background: "#FFFFFF",
       [`& fieldset`]: {
@@ -76,7 +76,7 @@ export const useStyles = makeStyles((theme) =>
 
     boxdesigntime: {
       width: "50%",
-      height: "44px",
+      lineHeight: "44px",
       fontSize: "24px",
       background: "#FFFFFF",
       [`& fieldset`]: {
